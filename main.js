c2175f98bccb157668e73feba12508bbec5419a6
Kaveesh Nayak <41531388+kaveeshnayak@users.noreply.github.com>
2024-11-20 13:48:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+
+function myFunction() {
+  let city=document.getElementById("textbox").value;
+  city=String(city)
+  fetch("https://api.waqi.info/feed/"+city+"/?token=892947dfecc7ba74e80c7299e85de6fd8e405172")
+  .then(res => res.json())
+  .then(data =>  {
+    console.log(data);
+    console.log(data["data"]["aqi"]);
+    let x=data["data"]["aqi"];
+    document.getElementById("richTextBox").value=String(x)
+  });
+
+}
+function need(){
+  var counter=0;
+  var x=document.getElementById("richTextBox").value;
+  x=parseInt(x);
+  if(x>100){
+    alert("You may need an air purifier!");
+  }
+  else if(x<100){
+      if(document.getElementById("dropdown1").value==1){
+        counter=counter+1;
+      }
+      if(document.getElementById("dropdown2").value==1){
+        counter=counter+1;
+      }
+      if(document.getElementById("dropdown3").value==1){
+        counter=counter+1;
+      }
+      if(document.getElementById("dropdown4").value==1){
+        counter=counter+1;
+      }
+      if(counter>2){
+        alert("You may need an air purifier!")
+      }
+      else{
+        alert("You may not need an air purifier!")
+      }
+  }
+
+}
